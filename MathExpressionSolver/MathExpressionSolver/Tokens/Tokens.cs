3473c8edbfdc353918379f800c092b8667216def
Petr Houška <houskape@outlook.com>
2014-12-08 18:50:59 +0100

    Added basic Token interface.

---
+++ MathExpressionSolver/MathExpressionSolver/Tokens/Tokens.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExpressionSolver.Tokens
+{
+
+    public interface IToken<T>
+    {
+        T ReturnValue();
+        int Priority { get; }
+    }
+
+    public abstract class Token<T> : IToken<T>
+    {
+        virtual public int Priority { get; protected set; }
+        abstract public T ReturnValue();
+    }
+}
