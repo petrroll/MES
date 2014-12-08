28dbb772e2717cb6145d752eb34ced127edba9ee
Petr Houška <houskape@outlook.com>
2014-12-08 19:01:39 +0100

    Added binary and unary Token examples.

--- MathExpressionSolver/MathExpressionSolver/Tokens/Tokens.cs
+++ MathExpressionSolver/MathExpressionSolver/Tokens/Tokens.cs
@@ -19,5 +19,58 @@ namespace MathExpressionSolver.Tokens
         abstract public T ReturnValue();
     }
 
-   
+    public abstract class UnToken<T> : Token<T>
+    {
+        public IToken<T> Child { get; set; }
+    }
+
+    public abstract class UnIntToken : Token<int> { }
+
+
+    public class IntToken : UnIntToken
+    {
+        public IntToken()
+        {
+            Priority = 1;
+        }
+
+        new public int Child { get; set; }
+
+        public override int ReturnValue()
+        {
+            return Child;
+        }
+
+        public override string ToString()
+        {
+            return Child.ToString();
+        }
+    }
+
+    public abstract class BinToken<T> : Token<T>
+    {
+        public IToken<T> LeftChild { get; set; }
+        public IToken<T> RightChild { get; set; }
+    }
+
+    public abstract class BinIntToken : BinToken<int> { }
+
+    public class MinusToken : BinIntToken
+    {
+        public MinusToken()
+        {
+            Priority = 2;
+        }
+
+        public override int ReturnValue()
+        {
+            return LeftChild.ReturnValue() - RightChild.ReturnValue();
+        }
+
+        public override string ToString()
+        {
+            return LeftChild.ToString() + " - " + RightChild.ToString();
+        }
+    }
+
 }
