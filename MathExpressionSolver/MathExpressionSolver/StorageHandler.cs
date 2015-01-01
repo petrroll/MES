04affa0b30758c3153e12a26cabdbcb66de9dc18
Petr Houška <houskape@outlook.com>
2015-01-01 21:35:37 +0100

    Improved extension methods.

---
+++ MathExpressionSolver/MathExpressionSolver/StorageHandler.cs
@@ -0,0 +1,22 @@
+﻿using MathExpressionSolver.Tokens;
+using System.Collections.Generic;
+
+
+namespace MathExpressionSolver
+{
+    class StorageHandler<T>
+    {
+        public Dictionary<string, T> Variables { get; private set; } = new Dictionary<string, T>();
+        public Dictionary<string, IToken<T>> Functions { get; private set; } = new Dictionary<string, IToken<T>>();
+
+        public void AddVariable(string variableName, T value)
+        {
+            Variables[variableName] = value;
+        }
+
+        public void AddFunction (string funcName, IToken<T> value)
+        {
+            Functions[funcName] = value;
+        }
+    }
+}
