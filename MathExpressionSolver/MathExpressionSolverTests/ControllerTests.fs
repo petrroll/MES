1aefdb435ebc0da594c9f145dbb7c779c8562a2d
Petr Houška <houskape@gmail.com>
2016-03-07 17:56:22 +0100

    Reworked all tests.

--- MathExpressionSolver/MathExpressionSolverTests/ControllerTests.fs
+++ MathExpressionSolver/MathExpressionSolverTests/ControllerTests.fs
@@ -26,7 +26,7 @@ module ControllerIntegrationTests =
         let factory = new DoubleTokenFactory()
         factory.CustomVariables <- customVariables
         factory.CustomFunctions <- customFunctions 
-
+        
         let tokenizer = new Tokenizer<double>() 
         tokenizer.TokenFactory <- factory 
 
@@ -39,78 +39,109 @@ module ControllerIntegrationTests =
 
         controller
 
-    let testInput (contr : Controller<double>) (expectedValue : string) (expression : string) = 
+    let bindController (controller : Controller<_>) testFunction expression =
+        controller.ExecuteExpressionSafe expression |> testFunction
+
+    let testInput (contr : Controller<_>) (expectedValue : string) (expression : string) = 
         let result = contr.ExecuteExpression expression
         result.ToString() |> should equal expectedValue
 
+    let testValue (result : Result<_>) (value : double) = 
+        result.Exception |> should be Null
+        result.Value |> should (equalWithin 0.01) value
+
+    let testAssigment (result : Result<_>) (value : double) (variableName : string) =
+        result.Exception |> should be Null
+        result.Value |> should (equalWithin 0.01) value
+        result.AdditionalData |> should equal variableName
+
+    let testNewFunction (result : Result<_>) (functionName : string) = 
+        result.Exception |> should be Null   
+        result.AdditionalData |> should equal functionName
+
+    let testException (result : Result<_>) (exceptionMessage : string) =
+        result.Exception.Message |> should equal exceptionMessage
         
     [<Fact>]
     let SimpleExpression() =
-        let controller = initController()
-        let result = controller.ReturnResult("2 + 3")
-        result |> should (equalWithin 0.1) 5
+        let valueTester = bindController (initController()) testValue
+        valueTester "2 + 3" 5.0
+        valueTester "2 + 2.2" 4.2
+        valueTester "1 + 2 / 3" (5.0/3.0)
 
     [<Fact>]
-    let ComplexExpressions() =
-        let testWithCurrInstance = testInput (initController())
-        testWithCurrInstance "-107,306989780239" "3*(7+7)/2-2*6/7- &&&  (&6 +&9) *8-  (2+ 2/3*(6+exp  (2*7-6* 2 ) - 8)+(2>1))"
-        testWithCurrInstance "-107,306989780239" "3*(7+7)/2-2*6/7-(6+9)*8-(2+2/3*(6+exp(2*7-6*2)-8)+(2>1))"
-
-        testWithCurrInstance "1" "3 > 1"
-        testWithCurrInstance "-5" "((2+1)-(3+1)*2)"
+    let ParserTests() =
+        let valueTester = bindController (initController()) testValue
+        valueTester "3*(7+7)/2-2*6/7- &&&  (&6 +&9) *8-  (2+ 2/3*(6+exp  (2*7-6* 2 ) - 8)+(2>1))" -107.306989780239
+        valueTester "3*(7+7)/2-2*6/7-(6+9)*8-(2+2/3*(6+exp(2*7-6*2)-8)+(2>1))" -107.306989780239
+    
+    [<Fact>]
+    let BracketsTests() =
+        let valueTester = bindController (initController()) testValue
+        valueTester "((2+1)-(3+1)*2)" -5.0
+        valueTester "(1 + 2) / 3" 1.0
 
     [<Fact>]
     let Variables() =
-        let testWithCurrInstance = testInput (initController())
-        testWithCurrInstance "a = 2,5" "a=(3/6*5)"
-        testWithCurrInstance "asdfsdf = 8" "asdfsdf=(5 + 3)"
+        let controller = initController ()
+        let valueTester = bindController (controller) testValue
+        let variableTester = bindController (controller) testAssigment
 
-        testWithCurrInstance "Pi = 3,4" "Pi = 3.4"
-        testWithCurrInstance "7,6" "3 - Pi + 8"
+        variableTester "a=(3/6*5)" 2.5 "a"
+        variableTester "b = 8" 8.0 "b"
 
-        testWithCurrInstance "2,5" "a"
-        testWithCurrInstance "8" "asdfsdf"
+        variableTester "Pi_approx = 3.4" 3.4 "Pi_approx"
+        valueTester "3 - Pi_approx + 8" 7.6
 
-        testWithCurrInstance "13,3824939607035" "exp(a) + asdfsdf - 2*Pi"
+        variableTester "c=(5 + 3)" 8.0 "c"
+        valueTester "c" 8.0
+
+        valueTester "exp(a) + c - 2*Pi_approx" 13.3824939607035
+
 
     [<Fact>]
     let IfsSimple() =
-        let testWithCurrInstance = testInput (initController())
-        testWithCurrInstance "2" "if(1;2;3)" 
-        testWithCurrInstance "3" "if(0;2;3)" 
+        let valueTester = bindController (initController()) testValue
+        valueTester "if(1;2;3)" 2.0
+        valueTester "if(0;2;3)" 3.0
 
     let prepareVariables (controller : Controller<double>) =
         controller.ExecuteExpression "a=(3/6*5)" |> ignore
-        controller.ExecuteExpression "asdfsdf=(5 + 3)" |> ignore
+        controller.ExecuteExpression "b=(5 + 3)" |> ignore
 
         controller.ExecuteExpression "Pi = 3.4"|> ignore
+        controller
 
     [<Fact>]
-    let IfsAdv() =
+    let AdvancedExpressions() =
+        let controller = prepareVariables (initController())
+        let valueTester = bindController controller testValue
+
+        valueTester "if((exp(100)> Pi)*2;exp(a) + b - 2*Pi;2*3-b)" 13.3824939607035
+        valueTester "(if((exp(100)> Pi)*2;exp(a) + b - 2*Pi;2*3-b))*2>1" 1.0
+
+    [<Fact>]
+    let FuncSimple() =
         let controller = initController()
-        prepareVariables controller
-        let testWithCurrInstance = testInput controller
+        let valueTester = bindController controller testValue
+        let funcTester = bindController controller testNewFunction
 
-        testWithCurrInstance "13,3824939607035" "if((exp(100)> Pi)*2;exp(a) + asdfsdf - 2*Pi;2*3-asdfsdf)"
-        testWithCurrInstance "1" "(if((exp(100)> Pi)*2;exp(a) + asdfsdf - 2*Pi;2*3-asdfsdf))*2>1" 
-
-    [<Fact>]
-    let FuncsSimple() =
-        let testWithCurrInstance = testInput (initController())
-
-        testWithCurrInstance "Function funA set." "funA(a;b) = a + b" 
-        testWithCurrInstance "7" "funA(2;5)"
+        funcTester "funA(a;b) = a + b" "funA"
+        valueTester "funA(2;5)" 7.0
 
     [<Fact>]
     let FuncsAdv() =
-        let contr = initController()
-        contr.ExecuteExpression("Pi = 3.4") |> ignore
-        let testWithCurrInstance = testInput contr
+        
+        let controller = initController()
+        controller.ExecuteExpression "Pi = 3.4" |> ignore
 
-        testWithCurrInstance "Function funB set." "funB(height_in_m; diameter_in_cm) = Pi * ((diameter_in_cm / 100) * (diameter_in_cm / 100)) * height_in_m"
+        let valueTester = bindController controller testValue
+        let funcTester = bindController controller testNewFunction
 
-        testWithCurrInstance "0,000963490199635007" "funB(Pi; sin(20))"
-        testWithCurrInstance "0,4352" "funB(20; 8)"
+        funcTester "funB(height_in_m; diameter_in_cm) = Pi * ((diameter_in_cm / 100) * (diameter_in_cm / 100)) * height_in_m" "funB"
+
+        valueTester "funB(Pi; sin(20))" 0.000963490199635007
+        valueTester "funB(20; 8)" 0.4352
 
 
          
